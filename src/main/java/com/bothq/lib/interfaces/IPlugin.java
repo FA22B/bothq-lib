418eb2c63ad349b3d5bf7046b7220bcb7390ae4e
Spencernator <150229110+Spencernator@users.noreply.github.com>
2024-03-07 11:11:33 +0100

    Added more methods to enable functionality

--- src/main/java/com/bothq/lib/interfaces/IPlugin.java
+++ src/main/java/com/bothq/lib/interfaces/IPlugin.java
@@ -1,17 +1,42 @@
 package com.bothq.lib.interfaces;
 
+import net.dv8tion.jda.api.JDA;
+
+/**
+ * The Plugin interface needed for the core framework to know where the entry point is, as well as handling
+ * other interactions between core framework and the plugin. Class instance will be created using Reflection.
+ * Constructor must be public accessible, without any parameters.
+ */
 public interface IPlugin {
     /**
      * Get the name of the plugin.
      *
      * @return The name of the plugin.
      */
-    public String getName();
+    String getName();
 
     /**
      * Get the description of the plugin.
      *
      * @return The description of the plugin.
      */
-    public String getDescription();
+    String getDescription();
+
+    /**
+     * Initializes the plugin by calling this method and passing the JDA instance to store for later use.
+     *
+     * @param jda The JDA instance.
+     */
+    void initialize(JDA jda);
+
+    /**
+     * Triggered once the plugin is loaded by the core framework and ready to be used. This does not automatically
+     * mean that JDA is already in ready state. Use DiscordEventListener along with ReadyEvent for that.
+     */
+    void pluginLoad();
+
+    /**
+     * Triggered once the plugin is unloaded by the core framework. Should be used to dispose all memory allocations.
+     */
+    void pluginUnload();
 }
