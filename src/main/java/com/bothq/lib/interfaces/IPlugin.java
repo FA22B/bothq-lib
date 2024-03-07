11d1081dcc7d381db955073645f025ff67a64b9f
Spencernator <150229110+Spencernator@users.noreply.github.com>
2024-03-07 09:29:30 +0100

    Refactor

--- src/main/java/com/bothq/lib/interfaces/IPlugin.java
+++ src/main/java/com/bothq/lib/interfaces/IPlugin.java
@@ -3,12 +3,14 @@ package com.bothq.lib.interfaces;
 public interface IPlugin {
     /**
      * Get the name of the plugin.
+     *
      * @return The name of the plugin.
      */
     public String getName();
 
     /**
      * Get the description of the plugin.
+     *
      * @return The description of the plugin.
      */
     public String getDescription();
