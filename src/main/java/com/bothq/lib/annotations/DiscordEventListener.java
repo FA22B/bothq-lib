24feef58b39d1d618e6b4c7e2e4acf8d12ce12c0
Spencernator <150229110+Spencernator@users.noreply.github.com>
2024-03-07 11:11:09 +0100

    Added annotation for easy JDA event handling

---
+++ src/main/java/com/bothq/lib/annotations/DiscordEventListener.java
@@ -0,0 +1,21 @@
+package com.bothq.lib.annotations;
+
+import com.bothq.lib.interfaces.IPlugin;
+import net.dv8tion.jda.api.events.GenericEvent;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The annotation used to listen to JDAs Events which implement the {@link GenericEvent} interface.
+ * This can only be used on methods within the {@link IPlugin} instance and static methods. Others can't be processed.
+ *
+ * @see <a href="https://jda.wiki/introduction/events-list/">List of Events</a>
+ */
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DiscordEventListener {
+    Class<GenericEvent>[] value() default {};
+}
