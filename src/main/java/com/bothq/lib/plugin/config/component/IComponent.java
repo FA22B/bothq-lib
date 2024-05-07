b8bc4044bc0773dd523a9e19a169b4bdf1e0b395
Spencernator <150229110+Spencernator@users.noreply.github.com>
2024-05-07 12:46:38 +0200

    Adjusted config handling

--- src/main/java/com/bothq/lib/plugin/config/component/IComponent.java
+++ src/main/java/com/bothq/lib/plugin/config/component/IComponent.java
@@ -3,7 +3,7 @@ package com.bothq.lib.plugin.config.component;
 import com.bothq.lib.plugin.config.IConfigurable;
 import com.bothq.lib.plugin.config.INameable;
 
-public interface IComponent<T> extends IUnselectedServerComponent<IComponent<T>>, IConfigurable, INameable {
+public interface IComponent<T> extends IConfigurable, INameable {
     T getValue();
 
     void setValue(T value);
--- src/main/java/com/bothq/lib/plugin/config/component/IUnselectedServerComponent.java
+++ src/main/java/com/bothq/lib/plugin/config/component/IUnselectedServerComponent.java
@@ -2,7 +2,7 @@ package com.bothq.lib.plugin.config.component;
 
 import com.bothq.lib.plugin.config.INameable;
 
-public interface IUnselectedServerComponent<T> extends INameable {
+public interface IUnselectedServerComponent<T extends IComponent<?>> extends INameable {
     String getUniqueId();
 
     T get(long serverId);
