a314561fc223d7a7c38d0d4e8b3dbafa0bf3f10f
Justus Sieweke <JSI@capricorn.de>
2024-05-29 12:28:55 +0200

    Restructured Config Components. Added Description to Config

--- src/main/java/com/bothq/lib/plugin/config/IConfig.java
+++ src/main/java/com/bothq/lib/plugin/config/IConfig.java
@@ -1,5 +1,6 @@
 package com.bothq.lib.plugin.config;
 
 public interface IConfig extends IConfigGroup {
+    String getDescription();
     IConfigGroup addConfigGroup(String uniqueId, String displayName);
 }
