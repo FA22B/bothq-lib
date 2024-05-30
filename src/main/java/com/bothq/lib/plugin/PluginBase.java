03d6f11c3f0f21b4a5ac9c1f40f2b77f776bfce9
Justus Sieweke <JSI@capricorn.de>
2024-05-30 15:56:17 +0200

    Restructured Components. Added Enabled functionality.
    IComboBoxServer removed elementIndex.
    IRadioBoxServer changed to be more in line with IComboBoxServer.
    Plugin IConfig now has an additional IEnabled field, to check whether it is currently enabled.

--- src/main/java/com/bothq/lib/plugin/PluginBase.java
+++ src/main/java/com/bothq/lib/plugin/PluginBase.java
@@ -1,11 +1,18 @@
 package com.bothq.lib.plugin;
 
+import com.bothq.lib.plugin.config.IConfig;
+import com.bothq.lib.plugin.config.enabled.IEnabled;
+import lombok.Getter;
 import net.dv8tion.jda.api.JDA;
 
 /**
  * An abstract class implementing {@link IPlugin} and providing basic functionality.
  */
 public abstract class PluginBase implements IPlugin {
+    @Getter
+    IEnabled enabled;
+
+
     /**
      * The JDA instance.
      */
@@ -17,5 +24,12 @@ public abstract class PluginBase implements IPlugin {
     public void initialize(JDA jda) {
         // Apply parameters
         this.jda = jda;
+    }
+
+
+
+    @Override
+    public void createConfig(IConfig config) {
+        this.enabled = config.getEnabled();
     }
 }
---
+++ src/main/java/com/bothq/lib/plugin/config/enabled/IEnabled.java
@@ -0,0 +1,5 @@
+package com.bothq.lib.plugin.config.enabled;
+
+public interface IEnabled {
+    IEnabledServer get(long serverId);
+}
